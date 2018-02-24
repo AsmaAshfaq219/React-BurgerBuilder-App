1e278197ed7657810020ce5a36211c6575e2d85f
AsmaAshfaq219 <asmaashfaq123@gmail.com>
2018-02-24 07:07:13 +0500

    Adding State and Dynamic output

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import Styles from './Burger.module.css';
-import Ingredient from './BurgerIngredient/BurgerIngredient';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import { Row, Col } from 'antd';
 
 export default (props) => {
+
+    // console.time('Random Person Code');
+    // const transformedIngredient2 = [];
+    // Object.keys(props.ingredients).forEach((v) => {
+    //     for (let i = 0; i < props.ingredients[v]; ++i)
+    //         transformedIngredient2.push(<BurgerIngredient type={v} key={v + i} />)
+    // });
+    // console.timeEnd('Random Person Code');
+
+    // console.time('My Code'); For performance check
+    let transformedIngredient = Object.keys(props.ingredients).map(ing => {
+        let ingredients = [];
+        for (let i = 0; i < props.ingredients[ing]; i++)
+            ingredients.push(<BurgerIngredient key={ing + i} type={ing} />);
+        return [...ingredients];
+    })
+    // console.timeEnd('My Code');
+    if (!transformedIngredient.join(''))
+        transformedIngredient = <p>Please Start Adding Ingredeints</p>
+
+    // console.time('Max Code');
+    // const transformedIngredient = Object.keys(props.ingredients)
+    //     .map(ing => [...Array(props.ingredients[ing])].map((_, i) =>
+    //         <BurgerIngredient key={ing + i} type={ing} />))
+    // console.timeEnd('Max Code')
     return (
         <Row>
             <Col xs={24}
@@ -11,13 +36,12 @@ export default (props) => {
                 md={{ span: 14, offset: 5 }}
                 lg={{ span: 12, offset: 6 }}
                 xl={{ span: 8, offset: 8 }}
-                xxl={{span: 6, offset: 9 }}
+                xxl={{ span: 6, offset: 9 }}
             >
                 <div className={Styles.Burger} >
-                    <Ingredient type={'breadTop'} />
-                    <Ingredient type={'cheese'} />
-                    <Ingredient type={'bacon'} />
-                    <Ingredient type={'breadBottom'} />
+                    <BurgerIngredient type={'breadTop'} />
+                    {transformedIngredient}
+                    <BurgerIngredient type={'breadBottom'} />
                 </div>
             </Col>
         </Row>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
-import {Button} from 'antd';
+import { Button } from 'antd';
 
 export default class BurgerBuilder extends Component {
-
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 1,
+            bacon: 2,
+            meat: 1
+        }
+    }
     render() {
         return (
             <Fragment>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Burger Controls
                     <Button>Hi</Button>
                 </div>
